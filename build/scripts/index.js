0e433bd17f3f757c2032467f4efdb4989d848372
Vladimir Fyodorov <vfyodorov@nes.ru>
2023-06-13 21:04:17 +0600

    makeMobHeaderTitle

--- build/scripts/index.js
+++ build/scripts/index.js
@@ -1,5 +1,12 @@
+window.addEventListener('DOMContentLoaded', () => {
+    highlightLinks();
+    makeMobMenu();
+    makeMobHeaderTitle();
+});
+
 function highlightLinks() {
     const href = window.location.href;
+    const mobHeader = document.querySelector('.header-mob-title');
 
     if (href.match('drawings') !== null) {
         const allLinks = document.querySelectorAll("a.inline-link");
@@ -11,6 +18,18 @@ function highlightLinks() {
         const links = document.querySelectorAll("a.inline-link[href='about.html']");
         allLinks.forEach(l => l.classList.remove('is-active'));
         links.forEach(l => l.classList.add('is-active'));
+    }
+}
+
+
+function makeMobHeaderTitle() {
+    const href = window.location.href;
+    const mobHeaderTitle = document.querySelector('.header-mob-title');
+
+    if (href.match('drawings') !== null) {
+        mobHeaderTitle.innerText = 'Drawings';
+    } else if (href.match('about') !== null) {
+        mobHeaderTitle.innerText = 'Biography';
     }
 }
 
@@ -27,8 +46,3 @@ function makeMobMenu() {
         header.classList.remove('is-mobile-menu-opened');
     });
 }
-
-window.addEventListener('DOMContentLoaded', () => {
-    highlightLinks();
-    makeMobMenu();
-});
