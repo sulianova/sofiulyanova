e866a7758c6a303cea7abfde1b7f2f5ca08ddcf5
sulianova <ulianova.sofia@gmail.com>
2023-06-19 12:55:16 +0600

    make lint & add image in drawings

--- build/scripts/index.js
+++ build/scripts/index.js
@@ -1,50 +1,49 @@
 window.addEventListener('DOMContentLoaded', () => {
-    highlightLinks();
-    makeMobMenu();
-    makeMobHeaderTitle();
+  highlightLinks();
+  makeMobMenu();
+  makeMobHeaderTitle();
 });
 
 function highlightLinks() {
-    const href = window.location.href;
-    const mobHeader = document.querySelector('.header-mob-title');
+  const href = window.location.href;
+  const mobHeader = document.querySelector('.header-mob-title');
 
-    if (href.match('drawings') !== null) {
-        const allLinks = document.querySelectorAll("a.inline-link");
-        const links = document.querySelectorAll("a.inline-link[href='drawings.html']");
-        allLinks.forEach(l => l.classList.remove('is-active'));
-        links.forEach(l => l.classList.add('is-active'));
-    } else if (href.match('about') !== null) {
-        const allLinks = document.querySelectorAll("a.inline-link");
-        const links = document.querySelectorAll("a.inline-link[href='about.html']");
-        allLinks.forEach(l => l.classList.remove('is-active'));
-        links.forEach(l => l.classList.add('is-active'));
-    }
+  if (href.match('drawings') !== null) {
+    const allLinks = document.querySelectorAll('a.inline-link');
+    const links = document.querySelectorAll("a.inline-link[href='drawings.html']");
+    allLinks.forEach((l) => l.classList.remove('is-active'));
+    links.forEach((l) => l.classList.add('is-active'));
+  } else if (href.match('about') !== null) {
+    const allLinks = document.querySelectorAll('a.inline-link');
+    const links = document.querySelectorAll("a.inline-link[href='about.html']");
+    allLinks.forEach((l) => l.classList.remove('is-active'));
+    links.forEach((l) => l.classList.add('is-active'));
+  }
 }
 
-
 function makeMobHeaderTitle() {
-    const href = window.location.href;
-    const mobHeaderTitle = document.querySelector('.header-mob-title');
+  const href = window.location.href;
+  const mobHeaderTitle = document.querySelector('.header-mob-title');
 
-    if (href.match('drawings') !== null) {
-        mobHeaderTitle.innerText = 'Drawings';
-    } else if (href.match('about') !== null) {
-        mobHeaderTitle.innerText = 'Biography';
-    }
+  if (href.match('drawings') !== null) {
+    mobHeaderTitle.innerText = 'Drawings';
+  } else if (href.match('about') !== null) {
+    mobHeaderTitle.innerText = 'Biography';
+  }
 }
 
 function makeMobMenu() {
-    const header = document.querySelector('.header');
-    const btnOpen = document.querySelector('.header-humburger');
-    const btnClose = document.querySelector('.header-mob-menu-controls');
+  const header = document.querySelector('.header');
+  const btnOpen = document.querySelector('.header-humburger');
+  const btnClose = document.querySelector('.header-mob-menu-controls');
 
-    btnOpen.addEventListener('click', () => {
-        header.classList.add('is-mobile-menu-opened');
-        document.body.style.overflow = 'hidden';
-    });
+  btnOpen.addEventListener('click', () => {
+    header.classList.add('is-mobile-menu-opened');
+    document.body.style.overflow = 'hidden';
+  });
 
-    btnClose.addEventListener('click', () => {
-        header.classList.remove('is-mobile-menu-opened');
-        document.body.style.overflow = null;
-    });
+  btnClose.addEventListener('click', () => {
+    header.classList.remove('is-mobile-menu-opened');
+    document.body.style.overflow = null;
+  });
 }
